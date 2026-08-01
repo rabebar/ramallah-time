3ce036f476a68150989c0c20c181d9867400e010
Rabebar <rabe.bar.a74@gmail.com>
2026-08-01 10:19:35 +0300

    Refine Moeen home visual hierarchy

--- static/moeen_exec/app.js
+++ static/moeen_exec/app.js
@@ -957,7 +957,7 @@ $("#moeenPaymentForm").onsubmit=async e=>{
 };
 if("serviceWorker" in navigator){
   navigator.serviceWorker
-    .register("/moeen-executive/sw.js?v=39",{updateViaCache:"none"})
+    .register("/moeen-executive/sw.js?v=40",{updateViaCache:"none"})
     .then(registration=>registration.update())
     .catch(()=>{});
 }
